baf7b8f3df87508e396368849fb6fbc9e1fb0364
Elizabeth Kolade <54336149+Lizzy223@users.noreply.github.com>
2024-11-15 08:08:00 +0100

    Create CustomTokens.sol

---
+++ CustomTokens.sol
@@ -0,0 +1,21 @@
+// In order to create an ERC20 token, you need the following:
+
+// - The Token’s Name
+// - The Token’s Symbol
+// - The Token’s Decimal Places
+// - The Total Number of Tokens in Circulation
+
+//Please set the below variables in your contract with relevant values
+
+string public constant name;
+string public constant symbol;
+uint8 public constant decimals;
+
+uint256 totalSupply_;
+constructo (uint256 total ) public {
+  totalSupply_ = total;
+  balances[msg.sender] = totalSupply_;
+}
+
+
+
